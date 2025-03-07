cf3e4d78060b129a00524270e680255985766dcc
Enjel Hutasoit <enjel.hutasoit@gmail.com>
2025-03-07 17:46:07 +0700

    Implement user registration form with gender selection and success/failure handling

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
-def hellow_world():
-    if request.method == "POST":
-        return render_template("greet.html", name=request.form.get("name"))
-    else:        
-      return render_template("index.html")
+GENDERS = [
+    "Female", 
+    "Male", 
+    "Prefer not to say"
+]
+
+@app.route("/")
+def index():
+      return render_template("index.html", genders=GENDERS)
+    
+@app.route("/register", methods=["POST"])
+def register():
+   if not request.form.get("name") or request.form.get("gender") not in GENDERS:
+      return render_template("failure.html")
+   return render_template("success.html")
